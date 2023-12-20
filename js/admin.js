093c4ffff4fc580ccc82827120d2b69f0112cd67
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2023-12-20 15:35:11 -0300

    Avance en la página de administración

--- js/admin.js
+++ js/admin.js
@@ -4,8 +4,17 @@ const divAdminFunc = document.getElementById('admin_func')
 const ulListaFuncPresentes = document.getElementById("lista_func_presentes")
 const ullistaUltimasFirmas = document.getElementById("lista_ultimas_firmas")
 const spanNumFuncPresentes = document.getElementById('num_func_presentes')
+const divMenuFuncionario = document.getElementById('menu_funcionario')
+const divMenu_registro = document.getElementById('menu_registro')
+const claseSubMenu= Array.from(document.getElementsByClassName('submenu'))
+const divRegistroFuncionario = document.getElementById('registro_funcionario')
+const formRegistroFuncionario = document.getElementById('form_reg_funcionario')
+
 
 const urlApiAdmin="http://localhost/RelojIFD/api/admin.php"
+
+divMenuFuncionario.style.height='0px'
+divRegistroFuncionario.style.height='0px'
 
 const firmas = {}
 
@@ -39,44 +48,45 @@ function obtenerFirmasTodas() {
     const datosConsulta = {}
     datosConsulta.modo = 0;
 
-    enviarAlServidor(datosConsulta,urlApiAdmin).
-        then(res=>{
-            //console.log(res)
-            limpiarListaUltFirmas()
+    if (divFuncPresentes.offsetParent != null) {
+        enviarAlServidor(datosConsulta,urlApiAdmin).
+            then(res=>{
+                //console.log(res)
+                limpiarListaUltFirmas()
 
-            firmas.lista = []
+                firmas.lista = []
 
-            res.respuesta.datos.forEach(firma => {
-                //console.log(firma)
-                firmas.lista.push(firma)
+                res.respuesta.datos.forEach(firma => {
+                    //console.log(firma)
+                    firmas.lista.push(firma)
 
-                const liFirma = document.createElement('li')
-                if (firma.tipo == 'entrada') {
-                    liFirma.classList.add('entrada')
-                }
-                else{
-                    liFirma.classList.add('salida')
-                }
+                    const liFirma = document.createElement('li')
+                    if (firma.tipo == 'entrada') {
+                        liFirma.classList.add('entrada')
+                    }
+                    else{
+                        liFirma.classList.add('salida')
+                    }
 
-                const pFuncionario = document.createElement('p')
-                pFuncionario.classList.add('funcionario')
-                pFuncionario.innerText = firma.nombre + ' ' +firma.apellido
-                liFirma.appendChild(pFuncionario)
+                    const pFuncionario = document.createElement('p')
+                    pFuncionario.classList.add('funcionario')
+                    pFuncionario.innerText = firma.nombre + ' ' +firma.apellido
+                    liFirma.appendChild(pFuncionario)
 
-                const pDescripcion = document.createElement('p')
-                pDescripcion.classList.add('descripcion')
-                pDescripcion.innerHTML = 
-                    "<span class='tipoFirma'>" + 
-                    firma.tipo.toUpperCase() + 
-                    "</span> a las <span class='horaEntrada'>" +
-                    firma.fechahora.split(' ')[1] +
-                    "</span>"
-                liFirma.appendChild(pDescripcion)
-                ullistaUltimasFirmas.appendChild(liFirma)
-            });
-            obtenerFuncionariosPresentes()
-        })
-
+                    const pDescripcion = document.createElement('p')
+                    pDescripcion.classList.add('descripcion')
+                    pDescripcion.innerHTML = 
+                        "<span class='tipoFirma'>" + 
+                        firma.tipo.toUpperCase() + 
+                        "</span> a las <span class='horaEntrada'>" +
+                        firma.fechahora.split(' ')[1] +
+                        "</span>"
+                    liFirma.appendChild(pDescripcion)
+                    ullistaUltimasFirmas.appendChild(liFirma)
+                });
+                obtenerFuncionariosPresentes()
+            })
+        }
     setTimeout(obtenerFirmasTodas,5000)
 }
 
@@ -139,6 +149,43 @@ function AbrirInicio() {
     divFuncPresentes.style.display="block"
     divUltimasFirmas.style.display="block"
     divAdminFunc.style.display="none"
+}
+
+function abrirMenuFuncionarios(modo) {
+    if (divMenuFuncionario.style.height=="0px") {
+        divMenuFuncionario.style.height="60px"
+        switch (modo) {
+            case "registro":
+                claseSubMenu.filter(function (sm) { return(sm.id=='menu_registro')})[0].style.display='block'
+                break;
+        
+            default:
+                break;
+        }
+    } else {
+        divMenuFuncionario.style.height="0px"
+        claseSubMenu.forEach(submenu => {
+            submenu.style.display='none'
+        });
+    }
+}
+
+
+function abrirFormularioRegistro(modo) {
+    if (divRegistroFuncionario.style.height=='0px') {
+        divRegistroFuncionario.style.height='160px'
+        switch (modo) {
+            case 'uno':
+                formRegistroFuncionario.style.display='block'
+                break;
+        
+            default:
+                break;
+        }
+    } else {
+        divRegistroFuncionario.style.height='0px'
+        formRegistroFuncionario.style.display='none'
+    }
 }
 
 
